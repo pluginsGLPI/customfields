57b35567dc471c21501611af2e5d1b4ae5b6450b
matt@opensourcegov.net <matt@opensourcegov.net@6f9ec73b-e5d2-495b-a2da-7e577d17d9e1>
2009-08-14 18:05:47 +0000

    update to version 1.1.1

---
+++ hook.php
@@ -0,0 +1,422 @@
+<?php
+/*
+ ---------------------------------------------------------------------- 
+ GLPI - Gestionnaire Libre de Parc Informatique 
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Sponsor: Oregon Dept. of Administrative Services, State Data Center
+// Original Author of file: Ryan Foster
+// Contact: Matt Hoover <dev@opensourcegov.net>
+// Project Website: http://www.opensourcegov.net
+// Purpose of file: Code for hooks, etc.
+// ----------------------------------------------------------------------
+
+//include_once ('inc/plugin_customfields.function.php');
+//include_once ('inc/plugin_customfields.class.php');
+
+// Define dropdown relations for use by GLPI
+function plugin_customfields_getDatabaseRelations()
+{
+	global $DB;
+	$plugin = new Plugin();
+
+	if ($plugin->isActivated("customfields"))
+	{
+		$relations=array();
+		$query="SELECT * FROM glpi_plugin_customfields_fields WHERE hidden=0 AND deleted=0 AND data_type='dropdown' ORDER BY device_type";
+		$result=$DB->query($query);
+		while ($data=$DB->fetch_assoc($result))
+		{
+			$relations[$data['dropdown_table']]=array(plugin_customfields_table($data['device_type'])=>$data['system_name']);
+		}
+
+		$entities=array();
+		$query="SELECT dropdown_table FROM glpi_plugin_customfields_dropdowns WHERE has_entities=1;";
+		$result=$DB->query($query);
+		while ($data=$DB->fetch_assoc($result))
+		{
+			$entities[$data['dropdown_table']]='FK_entities'; 
+		}
+		if(!empty($entities))
+			$relations['glpi_entities']=$entities;
+
+		return $relations;
+	}
+	else
+		return array();
+}
+
+// Define dropdown tables to be managed in GLPI
+function plugin_customfields_getDropdown()
+{
+	global $DB;
+	$plugin = new Plugin();
+
+	if ($plugin->isActivated("customfields"))
+	{
+		$dropdowns = array();
+
+		$query='SELECT * FROM glpi_plugin_customfields_dropdowns';
+		$result=$DB->query($query);
+		while ($data=$DB->fetch_assoc($result))
+		{
+			$dropdowns[$data['dropdown_table']]=$data['label'];
+		}
+		return $dropdowns;
+	}
+	else
+		return array();
+}
+
+/////////// SEARCH FUNCTIONS ////////////
+
+// Define search options for each device type that has custom fields.
+// 'Search options' are also used by GLPI for logging and mass updates.
+function plugin_customfields_getSearchOption()
+{
+	global $LANG,$DB;
+	$sopt=array();
+
+	$sopt[PLUGIN_CUSTOMFIELDS_TYPE]['common']=$LANG['plugin_customfields']['title'];
+
+	$query="SELECT f.*, dd.is_tree, cf.enabled FROM glpi_plugin_customfields as cf, glpi_plugin_customfields_fields AS f ".
+		" LEFT JOIN glpi_plugin_customfields_dropdowns AS dd ON dd.system_name=f.system_name ".
+		" WHERE f.device_type=cf.device_type ".
+		" ORDER BY f.device_type, f.sort_order, f.label";
+	$result=$DB->query($query);
+
+	$device_type='';
+	while ($data=$DB->fetch_assoc($result))
+	{
+		// Range 5200-7699 used by this plugin
+		$lpos = $data['sopt_pos'] + 5200; // first 1000 used for logging
+		$spos = $data['sopt_pos'] + 6200; // next 1000 used for searches
+		if($data['device_type']!=$device_type)
+		{
+			$mupos = 7200; // last 500 used for mass update 
+			$table = plugin_customfields_link_id_table($data['device_type']);
+			$table2 = plugin_customfields_table($data['device_type']);
+		}
+
+		$device_type=$data['device_type'];
+		if($data['deleted'] || $data['hidden'] || !$data['enabled']) // preserve names for log history
+		{
+			if(CUSTOMFIELDS_GLPI_PATCH_APPLIED)
+			{
+				$sopt[$device_type][$lpos]['name']=$data['label'];
+				$sopt[$device_type][$lpos]['field']='';
+				$sopt[$device_type][$lpos]['linkfield']='';
+				$sopt[$device_type][$lpos]['purpose']='log'; // an extra field used to clean search options
+			}
+		}
+		elseif($data['data_type']=='sectionhead')
+		{
+			$sopt[$device_type]['customfields_'.$data['system_name']]=$data['label'];
+		}
+		elseif($data['data_type']=='dropdown')
+		{
+			// search, logging, and mass update all work for dropdowns
+			$sopt[$device_type][$lpos]['table']=$data['dropdown_table'];
+			if($data['is_tree']==1)
+				$sopt[$device_type][$lpos]['field']='completename';
+			else
+				$sopt[$device_type][$lpos]['field']='name';
+			$sopt[$device_type][$lpos]['linkfield']=$data['system_name'];
+			$sopt[$device_type][$lpos]['name']=$data['label'];
+		}
+		else
+		{
+			// Note: Yes/No fields are included in search, logging, and mass update functionality. 
+			// In the GLPI core they are not usually included.
+
+			// For fields that aren't dropdowns, it is necessary to apply a patch 
+			// to enable logging and mass update functionality
+			if(CUSTOMFIELDS_GLPI_PATCH_APPLIED)
+			{
+				// for logging (these might need to be the first set of options)
+				$sopt[$device_type][$lpos]['table']=$table;
+				$sopt[$device_type][$lpos]['field']=$data['system_name'];
+				$sopt[$device_type][$lpos]['linkfield']='';
+				$sopt[$device_type][$lpos]['name']=$data['label'];
+				$sopt[$device_type][$lpos]['purpose']='log'; // an extra field used to clean search options
+
+				// for mass update
+				$mupos++;
+				$sopt[$device_type][$mupos]['table']=$table2;
+				$sopt[$device_type][$mupos]['field']=$data['system_name'];
+				$sopt[$device_type][$mupos]['linkfield']=$data['system_name'];
+				$sopt[$device_type][$mupos]['name']=$data['label'];
+				$sopt[$device_type][$mupos]['purpose']='update'; // an extra field used to clean search options
+			}
+			// for search
+			$sopt[$device_type][$spos]['table']=$table2;
+			$sopt[$device_type][$spos]['field']=$data['system_name'];
+			$sopt[$device_type][$spos]['linkfield']='ID';
+			$sopt[$device_type][$spos]['name']=$data['label'];
+			$sopt[$device_type][$spos]['purpose']='search'; // an extra field used to clean search options
+		}
+	}
+
+	return $sopt;
+}
+
+// Clean Search Options: Necessary for search to work properly if GLPI patch applied. 
+// Removes the search options that are used for different purposes.
+// This function requires the glpi patch in order to be called. See the patch directory for instructions.
+function plugin_customfields_cleanSearchOption($options, $action)
+{
+	if(!empty($options))
+	{
+		foreach($options as $ID => $value) 
+		{
+			if(is_array($value) && isset($value['purpose']))
+			{
+				// If action is 'r' we are cleaning before a search. 
+				// If action is 'w', we are cleaning before an update.
+				if ($value['purpose']=='log')
+					unset($options[$ID]);
+				elseif ($value['purpose']=='search' && $action=='w')
+					unset($options[$ID]);
+				elseif ($value['purpose']=='update' && $action=='r')
+					unset($options[$ID]);
+			}
+		}
+	}
+
+	return $options;
+}
+// Define how to join the tables when doing a search
+function plugin_customfields_addLeftJoin($type,$ref_table,$new_table,$linkfield,&$already_link_tables)
+{
+	$type_table = plugin_customfields_table($type);
+	if ($new_table==$type_table)
+	{
+		$out=addLeftJoin($type,$ref_table,$already_link_tables,$new_table,$linkfield);
+		return $out;
+	}
+	else // it is a custom dropdown
+	{
+		$out=addLeftJoin($type,$ref_table,$already_link_tables,$type_table,'ID');
+		$out.= " LEFT JOIN $new_table ON ($new_table.ID = $type_table.$linkfield) ";
+		return $out;
+	}
+}
+
+///////////// VARIOUS HOOKS /////////////////
+
+// Hook to process Mass Update & transfer
+function plugin_pre_item_update_customfields($data)
+{
+	global $ACTIVE_CUSTOMFIELDS_TYPES;
+
+	if (empty($ACTIVE_CUSTOMFIELDS_TYPES)) 
+		return $data;
+
+	// If update isn't set, then this is a mass update or transfer, not a regular update
+	if(!isset($data['update']) && !isset($data['_already_called_']) && in_array($data['_item_type_'],$ACTIVE_CUSTOMFIELDS_TYPES))
+	{
+		// mass update or tranfer, possibly affecting one of our custom fields
+		$updates=array();		
+		if(isset($data['FK_entities'])) // the item is being transfered to another entity
+		{
+			$updates=plugin_customfields_transferAllDropdowns($data['ID'],$data['_item_type_'],$data['FK_entities']);
+		}
+
+		$plugin_customfields = new plugin_customfields($data['_item_type_']);
+		$newdata=array_merge($updates,$data);
+		$newdata['_already_called_']=true; // prevents recurrsion
+		// The data may or may not be a custom field. At the moment we try an update regardless
+		$plugin_customfields->update($newdata);
+	}
+
+	return $data; // return the original data, not our additional data
+}
+
+// Hook done on add item case
+// If in Auto Activate mode, add a record for the custom fields when a device is added
+function plugin_item_add_customfields($parm)
+{
+	global $DB,$ACTIVE_CUSTOMFIELDS_TYPES;
+
+	if (CUSTOMFIELDS_AUTOACTIVATE && isset($parm['type']) && !empty($ACTIVE_CUSTOMFIELDS_TYPES))
+	{
+		if (in_array($parm['type'], $ACTIVE_CUSTOMFIELDS_TYPES))
+		{
+			$table=plugin_customfields_table($parm['type']);
+			$sql="INSERT INTO `$table` (ID) VALUES ('".intval($parm['ID'])."');";
+			$result = $DB->query($sql); 
+			return ($result ? true : false);
+		}
+	}
+	return false;
+}
+
+// Hook done on purge item case
+function plugin_item_purge_customfields($parm)
+{
+	global $DB,$ALL_CUSTOMFIELDS_TYPES;
+
+	// Must delete custom fields when main item is purged, even if custom fields for this device are currently disabled
+	if (in_array($parm['type'],$ALL_CUSTOMFIELDS_TYPES) && ($table=plugin_customfields_table($parm['type'])))
+	{
+		$sql="DELETE FROM `$table` WHERE ID = '".intval($parm['ID'])."' LIMIT 1;";
+		$result=$DB->query($sql);
+		return true;
+	}
+	else
+		return false;
+
+}
+
+// This function requires the glpi patch in order to be called. See the patch directory for instructions
+function plugin_customfields_MassiveActionsFieldsDisplay($type,$table,$field,$linkfield)
+{
+	global $DB;
+
+	$query="SELECT * FROM glpi_plugin_customfields_fields WHERE device_type='$type' AND system_name='$field';";
+	$result=$DB->query($query);
+	if ($data=$DB->fetch_assoc($result))
+	{
+		switch($data['data_type'])
+		{
+			case 'dropdown':
+				dropdownValue($data['dropdown_table'], $field, 1, $_SESSION['glpiactive_entity']);
+				break;
+			case 'yesno':
+				dropdownYesNo($field,0);
+				break;
+			case 'date':
+//				showCalendarForm('massiveaction_form',$field,'',true);
+				showDateFormItem($field,'',true,true);
+				break;
+			case 'money':			
+				echo '<input type="text" size="16" value="'.formatNumber(0,true).'" name="'.$field.'"/>';
+				break;
+			default:
+				autocompletionTextField($linkfield,$table,$field); 
+				break;
+		}
+		return true;
+	}
+	else
+		return false;
+}
+
+// Define headings added by the plugin -- determines if a tab should be shown or not
+function plugin_get_headings_customfields($type,$ID,$withtemplate)
+{
+	global $LANG, $ACTIVE_CUSTOMFIELDS_TYPES;
+
+	// Show the tab if Custom fields have been activated for this device type
+	if (!empty($ACTIVE_CUSTOMFIELDS_TYPES) && in_array($type,$ACTIVE_CUSTOMFIELDS_TYPES)) 
+	{
+		// template case
+		if ($withtemplate)
+			return array();
+		// Non template case
+		else 
+		{
+			return array(1 => $LANG['plugin_customfields']['title']);
+		}
+	}
+	else
+		return false;
+}
+// Define headings actions added by the plugin -- what happens when you click on the tab
+function plugin_headings_actions_customfields($type)
+{
+	global $ACTIVE_CUSTOMFIELDS_TYPES;
+
+	if (!empty($ACTIVE_CUSTOMFIELDS_TYPES) && in_array($type,$ACTIVE_CUSTOMFIELDS_TYPES))
+		return array(1 => 'plugin_headings_customfields');
+	else
+		return false;
+}
+
+// customfields of an action heading -- show the custom fields
+function plugin_headings_customfields($type,$ID,$withtemplate=0)
+{
+	if ($ID > -1)
+	{
+		echo '<div align="center">';
+		echo plugin_customfields_showAssociated($type,$ID);
+		echo '</div>';
+	}
+}
+
+// Define fields that can be updated with the data_injection plugin
+function plugin_customfields_data_injection_variables()
+{	
+	global $IMPORT_PRIMARY_TYPES, $DATA_INJECTION_MAPPING, $LANG, $IMPORT_TYPES,$DATA_INJECTION_INFOS,$DB;
+	$plugin = new Plugin();
+
+	if ($plugin->isActivated("customfields"))
+	{
+		$query="SELECT * FROM glpi_plugin_customfields_fields WHERE data_type <> 'sectionhead' AND deleted=0;";
+		$result=$DB->query($query);
+		while ($data=$DB->fetch_assoc($result))
+		{
+			$type=5200 + $data['device_type']; // this plugin uses the range 5200-7699
+			$field = $data['system_name'];
+			if($data['data_type']=='dropdown')
+			{
+				$DATA_INJECTION_MAPPING[$type][$field]['table'] = $data['dropdown_table'];
+				$DATA_INJECTION_MAPPING[$type][$field]['field'] = 'name';
+				$DATA_INJECTION_MAPPING[$type][$field]['linkfield'] = $field;
+				$DATA_INJECTION_INFOS[$type][$field]['linkfield'] = $field;
+				$DATA_INJECTION_MAPPING[$type][$field]['table_type'] = 'dropdown';
+				$DATA_INJECTION_INFOS[$type][$field]['table_type'] = 'dropdown';
+			}
+			else
+			{
+				$DATA_INJECTION_MAPPING[$type][$field]['table'] = plugin_customfields_table($type);
+				$DATA_INJECTION_MAPPING[$type][$field]['field'] = $field;
+			}
+			$DATA_INJECTION_MAPPING[$type][$field]['name'] = $data['label'];
+			switch($data['data_type'])
+			{
+				case 'number': 
+				case 'yesno': $DATA_INJECTION_MAPPING[$type][$field]['type'] = 'integer'; break;
+				case 'date': $DATA_INJECTION_MAPPING[$type][$field]['type'] = 'date'; break;
+				case 'money': $DATA_INJECTION_MAPPING[$type][$field]['type'] = 'float'; break;
+				case 'text':
+				case 'notes':
+					$DATA_INJECTION_MAPPING[$type][$field]['table_type'] = 'multitext';
+					$DATA_INJECTION_INFOS[$type][$field]['table_type'] = 'multitext';
+					$DATA_INJECTION_MAPPING[$type][$field]['type'] = 'text'; 
+					break;
+				default: $DATA_INJECTION_MAPPING[$type][$field]['type'] = 'text'; 
+			}
+			$DATA_INJECTION_INFOS[$type][$field]['table'] = $DATA_INJECTION_MAPPING[$type][$field]['table']; 
+			$DATA_INJECTION_INFOS[$type][$field]['field'] = $DATA_INJECTION_MAPPING[$type][$field]['field'];
+			$DATA_INJECTION_INFOS[$type][$field]['name'] = $DATA_INJECTION_MAPPING[$type][$field]['name'];
+			$DATA_INJECTION_INFOS[$type][$field]['type'] = $DATA_INJECTION_MAPPING[$type][$field]['type'];
+		}
+	}
+}
+
+?>
---
+++ locales/en_GB.php
@@ -0,0 +1,137 @@
+<?php
+/*
+
+   ----------------------------------------------------------------------
+   GLPI - Gestionnaire Libre de Parc Informatique
+   Copyright (C) 2003-2008 by the INDEPNET Development Team.
+
+   http://indepnet.net/   http://glpi-project.org/
+
+   ----------------------------------------------------------------------
+   LICENSE
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License (GPL)
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   To read the license please visit http://www.gnu.org/copyleft/gpl.html
+   ----------------------------------------------------------------------
+// Sponsor: Oregon Dept. of Administrative Services, State Data Center
+// Original Author of file: Ryan Foster
+// Contact: Matt Hoover <dev@opensourcegov.net>
+// Project Website: http://www.opensourcegov.net
+// Purpose of file: Language file
+----------------------------------------------------------------------
+ */
+
+$title = 'Custom Fields';
+
+//English
+
+$LANG['plugin_customfields']['title'] = $title;
+
+// General
+$LANG['plugin_customfields']['Enabled'] = 'Enabled';
+$LANG['plugin_customfields']['Disabled'] = 'Disabled';
+$LANG['plugin_customfields']['Enable'] = 'Enable';
+$LANG['plugin_customfields']['Disable'] = 'Disable';
+$LANG['plugin_customfields']['Device_Type'] = 'Device Type';
+$LANG['plugin_customfields']['Status'] = 'Status';
+$LANG['plugin_customfields']['Label'] = 'Label';
+$LANG['plugin_customfields']['System_Name'] = 'System Name';
+$LANG['plugin_customfields']['Update_Custom_Fields'] = 'Update '.$title;
+$LANG['plugin_customfields']['delete_warning'] = '(Warning: Can\'t undo!)';
+
+// Manage Custom Fields
+$LANG['plugin_customfields']['Manage_Custom_Fields'] = 'Manage '.$title;
+$LANG['plugin_customfields']['Type'] = $LANG['common'][17];
+$LANG['plugin_customfields']['Sort'] = 'Sort';
+$LANG['plugin_customfields']['Hidden'] = 'Hidden';
+$LANG['plugin_customfields']['no_cf_yet'] = 'No custom fields defined yet. Add them below.';
+$LANG['plugin_customfields']['Add_New_Field'] = 'Add New Field';
+$LANG['plugin_customfields']['Clone_Field'] = 'Clone Field (from another device type)';
+$LANG['plugin_customfields']['Field'] = 'Field';
+$LANG['plugin_customfields']['Add_Custom_Dropdown'] = 'Add Custom Dropdown';
+$LANG['plugin_customfields']['Dropdown_Name'] = 'Dropdown Name';
+$LANG['plugin_customfields']['status_of_cf'] = 'Status of '.$title;
+$LANG['plugin_customfields']['Activate_Custom_Fields'] = 'Activate '.$title;
+$LANG['plugin_customfields']['add_fields_first'] = 'You must add data fields before you can enable custom fields for this device type.';
+$LANG['plugin_customfields']['cf_enabled'] = 'Custom fields have been enabled for this device type.';
+$LANG['plugin_customfields']['cf_disabled'] = 'Custom fields have been disabled for this device type.';
+$LANG['plugin_customfields']['Custom_Field'] = 'Custom Field'; // the default name for a field if left blank
+
+// Manage Custom Dropdowns
+$LANG['plugin_customfields']['Manage_Custom_Dropdowns'] = 'Manage Custom Dropdowns';
+$LANG['plugin_customfields']['Back_to_Manage'] = 'Back to '.$LANG['plugin_customfields']['Manage_Custom_Fields'];
+$LANG['plugin_customfields']['Uses_Entities'] = 'Uses Entities';
+$LANG['plugin_customfields']['Tree_Structure'] = 'Tree Structure';
+$LANG['plugin_customfields']['Used_by_NNN_devices'] = 'Used by NNN device(s)'; // IMPORTANT: Use NNN where the number should go
+$LANG['plugin_customfields']['no_dd_yet'] = 'No custom dropdowns defined yet. Add them below.';
+$LANG['plugin_customfields']['Add_New_Dropdown'] = 'Add New Dropdown';
+$LANG['plugin_customfields']['Custom_Dropdown'] = 'Custom Dropdown'; //the default name of a custom dropdown
+
+// Data Types
+$LANG['plugin_customfields']['dropdown'] = 'Dropdown';
+$LANG['plugin_customfields']['general'] = 'General';
+$LANG['plugin_customfields']['text'] = 'Text';
+$LANG['plugin_customfields']['text_explained'] = 'Text (multiple lines)';
+$LANG['plugin_customfields']['notes'] = 'Notes';
+$LANG['plugin_customfields']['notes_explained'] = 'Notes (large text area)';
+$LANG['plugin_customfields']['date'] = 'Date';
+$LANG['plugin_customfields']['number'] = 'Number';
+$LANG['plugin_customfields']['money'] = 'Money';
+$LANG['plugin_customfields']['yesno'] = 'Yes/No';
+$LANG['plugin_customfields']['sectionhead'] = 'Section Header';
+
+// Device Types
+$LANG['plugin_customfields']['device_type'][COMPUTER_TYPE]  = $LANG['Menu'][0];
+$LANG['plugin_customfields']['device_type'][NETWORKING_TYPE]= $LANG['Menu'][1];
+$LANG['plugin_customfields']['device_type'][PRINTER_TYPE]   = $LANG['Menu'][2];
+$LANG['plugin_customfields']['device_type'][MONITOR_TYPE]   = $LANG['Menu'][3];
+$LANG['plugin_customfields']['device_type'][PERIPHERAL_TYPE]= $LANG['Menu'][16];
+$LANG['plugin_customfields']['device_type'][SOFTWARE_TYPE]  = $LANG['Menu'][4];
+$LANG['plugin_customfields']['device_type'][PHONE_TYPE]     = $LANG['Menu'][34];
+$LANG['plugin_customfields']['device_type'][CARTRIDGE_TYPE] = $LANG['Menu'][21];
+$LANG['plugin_customfields']['device_type'][CONSUMABLE_TYPE]= $LANG['Menu'][32];
+$LANG['plugin_customfields']['device_type'][CONTACT_TYPE]   = $LANG['Menu'][22];
+$LANG['plugin_customfields']['device_type'][ENTERPRISE_TYPE]= $LANG['Menu'][23];
+$LANG['plugin_customfields']['device_type'][CONTRACT_TYPE]  = $LANG['Menu'][25];
+$LANG['plugin_customfields']['device_type'][DOCUMENT_TYPE]  = $LANG['Menu'][27];
+$LANG['plugin_customfields']['device_type'][TRACKING_TYPE]  = $LANG['Menu'][5];
+$LANG['plugin_customfields']['device_type'][USER_TYPE]      = $LANG['Menu'][14];
+$LANG['plugin_customfields']['device_type'][NETWORKING_PORT_TYPE] = $LANG['networking'][6];
+$LANG['plugin_customfields']['device_type'][COMPUTERDISK_TYPE] = $LANG['computers'][8]; 
+$LANG['plugin_customfields']['device_type'][DEVICE_TYPE]    = $LANG['title'][30]; 
+
+$LANG['plugin_customfields']['component_type'][MOBOARD_DEVICE]   = $LANG['devices'][5]; 
+$LANG['plugin_customfields']['component_type'][PROCESSOR_DEVICE] = $LANG['devices'][4]; 
+$LANG['plugin_customfields']['component_type'][RAM_DEVICE]       = $LANG['devices'][6]; 
+$LANG['plugin_customfields']['component_type'][HDD_DEVICE]       = $LANG['devices'][1]; 
+$LANG['plugin_customfields']['component_type'][NETWORK_DEVICE]   = $LANG['devices'][3]; 
+$LANG['plugin_customfields']['component_type'][DRIVE_DEVICE]     = $LANG['devices'][19]; 
+$LANG['plugin_customfields']['component_type'][CONTROL_DEVICE]   = $LANG['devices'][20]; 
+$LANG['plugin_customfields']['component_type'][GFX_DEVICE]       = $LANG['devices'][2]; 
+$LANG['plugin_customfields']['component_type'][SND_DEVICE]       = $LANG['devices'][7]; 
+$LANG['plugin_customfields']['component_type'][PCI_DEVICE]       = $LANG['devices'][21];
+$LANG['plugin_customfields']['component_type'][CASE_DEVICE]      = $LANG['devices'][22]; 
+$LANG['plugin_customfields']['component_type'][POWER_DEVICE]     = $LANG['devices'][23]; 
+
+// Setup
+$LANG['plugin_customfields']['setup'][2] = 'This plugin requires GLPI version 0.72 or higher';
+$LANG['plugin_customfields']['setup'][3] = 'Setup of '.$title.' Plugin';
+$LANG['plugin_customfields']['setup'][4] = 'Existing custom field data was found on your system. This data has been restored.';
+$LANG['plugin_customfields']['setup'][5] = 'Data from an older version of '.$title.' was found. This data will be upgraded when you activate this plugin.';
+$LANG['plugin_customfields']['setup'][7] = 'The plugin files are not compatable with the existing data. Please upgrade your plugin files.';
+$LANG['plugin_customfields']['setup'][8] = 'Custom Fields has been uninstalled, but your custom field data has not been removed.';
+$LANG['plugin_customfields']['setup'][9] = 'Click here to delete all custom field data.';
+$LANG['plugin_customfields']['setup'][10] = 'Are you sure you want to delete all custom field data?';
+$LANG['plugin_customfields']['setup'][11] = 'Instructions';
+//$LANG['plugin_customfields']['setup'][12] = 'FAQ';
+//$LANG['plugin_customfields']['setup'][14] = 'Please change to "Root Entity (Show all)" before installing this plugin';
+?>
