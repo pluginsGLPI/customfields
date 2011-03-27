32ae1515fa31beb10d2364fe1fd6c928e3805956
yllen <yllen@6f9ec73b-e5d2-495b-a2da-7e577d17d9e1>
2011-03-27 12:18:36 +0000

    completed previous

--- install/plugin_customfields.changes-116.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `glpi_plugin_customfields_fields` CHANGE `system_name` `system_name` varchar(40) collate utf8_unicode_ci default NULL;
-ALTER TABLE `glpi_plugin_customfields_fields` CHANGE `label` `label` varchar(70) collate utf8_unicode_ci default NULL;
-ALTER TABLE `glpi_plugin_customfields_fields` CHANGE `default_value` `default_value` varchar(255) collate utf8_unicode_ci default NULL;
-ALTER TABLE `glpi_plugin_customfields_fields` CHANGE `dropdown_table` `dropdown_table` varchar(255) collate utf8_unicode_ci default NULL;
-ALTER TABLE `glpi_plugin_customfields_dropdowns` CHANGE `system_name` `system_name` varchar(40) collate utf8_unicode_ci default NULL;
-ALTER TABLE `glpi_plugin_customfields_dropdowns` CHANGE `label` `label` varchar(70) collate utf8_unicode_ci default NULL;
-ALTER TABLE `glpi_plugin_customfields_dropdowns` CHANGE `dropdown_table` `dropdown_table` varchar(255) collate utf8_unicode_ci default NULL;
-UPDATE `glpi_plugin_customfields_fields` SET `default_value`=NULL WHERE `default_value`='';
-
