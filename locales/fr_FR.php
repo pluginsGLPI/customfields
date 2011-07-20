d9214d0103459f243f75545295b3fd2a639223b8
Protux <Protux@6f9ec73b-e5d2-495b-a2da-7e577d17d9e1>
2011-07-20 11:58:34 +0000

    Added fr_FR missing strings from 078 branche

--- locales/fr_FR.php
+++ locales/fr_FR.php
@@ -47,14 +47,17 @@ $LANG['plugin_customfields']['Status'] = 'Statut';
 $LANG['plugin_customfields']['Label'] = 'Libellé';
 $LANG['plugin_customfields']['System_Name'] = 'Nom interne DB';
 $LANG['plugin_customfields']['Update_Custom_Fields'] = 'Actualiser '.$title;
+$LANG['plugin_customfields']['Update_Financial_CF'] = 'mettre à jour les informations financières CF';
 $LANG['plugin_customfields']['delete_warning'] = '(Attention: annulation impossible!)';
 
 // Manage Custom Fields
 $LANG['plugin_customfields']['Manage_Custom_Fields'] = 'Gérer les '.$title;
 $LANG['plugin_customfields']['Type'] = $LANG['common'][17];
+$LANG['plugin_customfields']['Location'] = 'Lieu';
 $LANG['plugin_customfields']['Sort'] = 'Tri';
 $LANG['plugin_customfields']['Required'] = 'Required';
 $LANG['plugin_customfields']['Restricted'] = 'Restricted';
+$LANG['plugin_customfields']['Unique'] = 'Unique';
 $LANG['plugin_customfields']['Entities'] = 'Entities';
 $LANG['plugin_customfields']['no_cf_yet'] = 'Aucun champ personnalisé n\'existe. Ajoutez-en ci-dessous.';
 $LANG['plugin_customfields']['Add_New_Field'] = 'Ajouter un nouveau champ';
@@ -68,6 +71,7 @@ $LANG['plugin_customfields']['add_fields_first'] = 'Vous devez ajouter des champ
 $LANG['plugin_customfields']['cf_enabled'] = 'Les Champs personnalisés sont activés pour ce type de composant.';
 $LANG['plugin_customfields']['cf_disabled'] = 'Les Champs personnalisés sont désactivés pour ce type de composant.';
 $LANG['plugin_customfields']['Custom_Field'] = 'Champ personnalisé'; // the default name for a field if left blank
+$LANG['plugin_customfields']['multiselect_note'] = 'Si vous créez une multisélection de champs, mettez nom_table.nom_champ dans le champ Nom System.';
 
 // Manage Custom Dropdowns
 $LANG['plugin_customfields']['Manage_Custom_Dropdowns'] = 'Gérer les listes déroulantes';
@@ -91,6 +95,44 @@ $LANG['plugin_customfields']['number'] = 'Nombre';
 $LANG['plugin_customfields']['money'] = 'Monétaire';
 $LANG['plugin_customfields']['yesno'] = 'Oui/Non';
 $LANG['plugin_customfields']['sectionhead'] = 'Entête de section';
+$LANG['plugin_customfields']['multiselect'] = 'Multi sélection';
+
+// Device Types
+$LANG['plugin_customfields']['device_type'][COMPUTER_TYPE]  = $LANG['Menu'][0];
+$LANG['plugin_customfields']['device_type'][NETWORKING_TYPE]= $LANG['Menu'][1];
+$LANG['plugin_customfields']['device_type'][PRINTER_TYPE]   = $LANG['Menu'][2];
+$LANG['plugin_customfields']['device_type'][MONITOR_TYPE]   = $LANG['Menu'][3];
+$LANG['plugin_customfields']['device_type'][PERIPHERAL_TYPE]= $LANG['Menu'][16];
+$LANG['plugin_customfields']['device_type'][SOFTWARE_TYPE]  = $LANG['Menu'][4];
+$LANG['plugin_customfields']['device_type'][PHONE_TYPE]     = $LANG['Menu'][34];
+$LANG['plugin_customfields']['device_type'][CARTRIDGE_TYPE] = $LANG['Menu'][21];
+$LANG['plugin_customfields']['device_type'][CONSUMABLE_TYPE]= $LANG['Menu'][32];
+$LANG['plugin_customfields']['device_type'][CONTACT_TYPE]   = $LANG['Menu'][22];
+$LANG['plugin_customfields']['device_type'][ENTERPRISE_TYPE]= $LANG['Menu'][23];
+$LANG['plugin_customfields']['device_type'][CONTRACT_TYPE]  = $LANG['Menu'][25];
+$LANG['plugin_customfields']['device_type'][DOCUMENT_TYPE]  = $LANG['Menu'][27];
+$LANG['plugin_customfields']['device_type'][TRACKING_TYPE]  = $LANG['Menu'][5];
+$LANG['plugin_customfields']['device_type'][USER_TYPE]      = $LANG['Menu'][14];
+$LANG['plugin_customfields']['device_type'][GROUP_TYPE]     = $LANG['Menu'][36];
+$LANG['plugin_customfields']['device_type'][ENTITY_TYPE]    = $LANG['Menu'][37];
+$LANG['plugin_customfields']['device_type'][NETWORKING_PORT_TYPE] = $LANG['networking'][6];
+$LANG['plugin_customfields']['device_type'][COMPUTERDISK_TYPE] = $LANG['computers'][8];
+$LANG['plugin_customfields']['device_type'][SOFTWAREVERSION_TYPE] = 'Software Versions';
+$LANG['plugin_customfields']['device_type'][SOFTWARELICENSE_TYPE] = 'Software License';
+$LANG['plugin_customfields']['device_type'][DEVICE_TYPE]    = $LANG['title'][30];
+
+$LANG['plugin_customfields']['component_type'][MOBOARD_DEVICE]   = $LANG['devices'][5];
+$LANG['plugin_customfields']['component_type'][PROCESSOR_DEVICE] = $LANG['devices'][4];
+$LANG['plugin_customfields']['component_type'][RAM_DEVICE]       = $LANG['devices'][6];
+$LANG['plugin_customfields']['component_type'][HDD_DEVICE]       = $LANG['devices'][1];
+$LANG['plugin_customfields']['component_type'][NETWORK_DEVICE]   = $LANG['devices'][3];
+$LANG['plugin_customfields']['component_type'][DRIVE_DEVICE]     = $LANG['devices'][19];
+$LANG['plugin_customfields']['component_type'][CONTROL_DEVICE]   = $LANG['devices'][20];
+$LANG['plugin_customfields']['component_type'][GFX_DEVICE]       = $LANG['devices'][2];
+$LANG['plugin_customfields']['component_type'][SND_DEVICE]       = $LANG['devices'][7];
+$LANG['plugin_customfields']['component_type'][PCI_DEVICE]       = $LANG['devices'][21];
+$LANG['plugin_customfields']['component_type'][CASE_DEVICE]      = $LANG['devices'][22];
+$LANG['plugin_customfields']['component_type'][POWER_DEVICE]     = $LANG['devices'][23];
 
 // Setup
 $LANG['plugin_customfields']['setup'][1] = 'Aucun champ restreint';
