8a1696d912d3fa14c00975f17ea7e0a30cb36380
rfoster <rfoster@6f9ec73b-e5d2-495b-a2da-7e577d17d9e1>
2011-07-20 11:09:03 +0000

    Update French file also

--- locales/es_ES.php
+++ locales/es_ES.php
@@ -88,44 +88,6 @@ $LANG['plugin_customfields']['money']           = 'Dinero';
 $LANG['plugin_customfields']['yesno']           = 'Sí/No';
 $LANG['plugin_customfields']['sectionhead']     = 'Cabecera de sección';
 
-// Device Types
-$LANG['plugin_customfields']['device_type']['Computer']           = $LANG['Menu'][0];
-$LANG['plugin_customfields']['device_type']['NetworkEquipment']   = $LANG['Menu'][1];
-$LANG['plugin_customfields']['device_type']['Printer']            = $LANG['Menu'][2];
-$LANG['plugin_customfields']['device_type']['Monitor']            = $LANG['Menu'][3];
-$LANG['plugin_customfields']['device_type']['Peripheral']         = $LANG['Menu'][16];
-$LANG['plugin_customfields']['device_type']['Software']           = $LANG['Menu'][4];
-$LANG['plugin_customfields']['device_type']['Phone']              = $LANG['Menu'][34];
-$LANG['plugin_customfields']['device_type']['CartridgeItem']      = $LANG['Menu'][21];
-$LANG['plugin_customfields']['device_type']['ConsumableItem']     = $LANG['Menu'][32];
-$LANG['plugin_customfields']['device_type']['Contact']            = $LANG['Menu'][22];
-$LANG['plugin_customfields']['device_type']['Supplier']           = $LANG['Menu'][23];
-$LANG['plugin_customfields']['device_type']['Contract']           = $LANG['Menu'][25];
-$LANG['plugin_customfields']['device_type']['Document']           = $LANG['Menu'][27];
-$LANG['plugin_customfields']['device_type']['Ticket']             = $LANG['Menu'][5];
-$LANG['plugin_customfields']['device_type']['User']               = $LANG['Menu'][14];
-$LANG['plugin_customfields']['device_type']['Group']              = $LANG['Menu'][36];
-$LANG['plugin_customfields']['device_type']['Entity']             = $LANG['Menu'][37];
-$LANG['plugin_customfields']['device_type']['NetworkPort']        = $LANG['networking'][6];
-$LANG['plugin_customfields']['device_type']['ComputerDisk']       = $LANG['computers'][8];
-$LANG['plugin_customfields']['device_type']['SoftwareVersion']    = 'Software Versions';
-$LANG['plugin_customfields']['device_type']['SoftwareLicense']    = 'Software License';
-$LANG['plugin_customfields']['device_type']['Device']             = $LANG['title'][30];
-$LANG['plugin_customfields']['device_type']['Infocom']            = 'Information financières';
-$LANG['plugin_customfields']['device_type']['DeviceMotherboard']  = $LANG['devices'][5];
-$LANG['plugin_customfields']['device_type']['DeviceProcessor']    = $LANG['devices'][4];
-$LANG['plugin_customfields']['device_type']['DeviceMemory']       = $LANG['devices'][6];
-$LANG['plugin_customfields']['device_type']['DeviceHardDrive']    = $LANG['devices'][1];
-$LANG['plugin_customfields']['device_type']['DeviceNetworkCard']  = $LANG['devices'][3];
-$LANG['plugin_customfields']['device_type']['DeviceDrive']        = $LANG['devices'][19];
-$LANG['plugin_customfields']['device_type']['DeviceControl']      = $LANG['devices'][20];
-$LANG['plugin_customfields']['device_type']['DeviceGraphicCard']  = $LANG['devices'][2];
-$LANG['plugin_customfields']['device_type']['DeviceSoundCard']    = $LANG['devices'][7];
-$LANG['plugin_customfields']['device_type']['DevicePci']          = $LANG['devices'][21];
-$LANG['plugin_customfields']['device_type']['DeviceCase']         = $LANG['devices'][22];
-$LANG['plugin_customfields']['device_type']['DevicePowerSupply']  = $LANG['devices'][23];
-
-
 // Setup
 $LANG['plugin_customfields']['setup'][1] = 'Aucun champ restreint';
 $LANG['plugin_customfields']['setup'][2] = 'Este plugin requiere la version 0.78  o mayor de GLPI.';
@@ -139,4 +101,10 @@ $LANG['plugin_customfields']['setup'][10] = '¿Está seguro de que quiere borrar
 $LANG['plugin_customfields']['setup'][11] = 'Instrucciones';
 //$LANG['plugin_customfields']['setup'][12] = 'FAQ';
 //$LANG['plugin_customfields']['setup'][14] = 'Please change to "Root Entity (Show all)" before installing this plugin';
+
+// Device Types
+$LANG['plugin_customfields']['device_type']['SoftwareVersion'] = 'Software Versions';
+$LANG['plugin_customfields']['device_type']['SoftwareLicense'] = 'Software License';
+include('devices.php');
+$LANG['plugin_customfields']['device_type']['Infocom']            = 'Information financières';
 ?>
--- locales/fr_FR.php
+++ locales/fr_FR.php
@@ -92,43 +92,6 @@ $LANG['plugin_customfields']['money'] = 'Monétaire';
 $LANG['plugin_customfields']['yesno'] = 'Oui/Non';
 $LANG['plugin_customfields']['sectionhead'] = 'Entête de section';
 
-// Device Types
-$LANG['plugin_customfields']['device_type']['Computer']        = $LANG['Menu'][0];
-$LANG['plugin_customfields']['device_type']['NetworkEquipment']= $LANG['Menu'][1];
-$LANG['plugin_customfields']['device_type']['Printer']         = $LANG['Menu'][2];
-$LANG['plugin_customfields']['device_type']['Monitor']         = $LANG['Menu'][3];
-$LANG['plugin_customfields']['device_type']['Peripheral']      = $LANG['Menu'][16];
-$LANG['plugin_customfields']['device_type']['Software']        = $LANG['Menu'][4];
-$LANG['plugin_customfields']['device_type']['Phone']           = $LANG['Menu'][34];
-$LANG['plugin_customfields']['device_type']['Cartridge']       = $LANG['Menu'][21];
-$LANG['plugin_customfields']['device_type']['Consumable']      = $LANG['Menu'][32];
-$LANG['plugin_customfields']['device_type']['Contact']         = $LANG['Menu'][22];
-$LANG['plugin_customfields']['device_type']['Supplier']        = $LANG['Menu'][23];
-$LANG['plugin_customfields']['device_type']['Contract']        = $LANG['Menu'][25];
-$LANG['plugin_customfields']['device_type']['Document']        = $LANG['Menu'][27];
-$LANG['plugin_customfields']['device_type']['Ticket']          = $LANG['Menu'][5];
-$LANG['plugin_customfields']['device_type']['User']            = $LANG['Menu'][14];
-$LANG['plugin_customfields']['device_type']['Group']           = $LANG['Menu'][36];
-$LANG['plugin_customfields']['device_type']['Entity']          = $LANG['Menu'][37];
-$LANG['plugin_customfields']['device_type']['NetworkPort']     = $LANG['networking'][6];
-$LANG['plugin_customfields']['device_type']['ComputerDisk']    = $LANG['computers'][8];
-$LANG['plugin_customfields']['device_type']['SoftwareVersion'] = 'Software Versions';
-$LANG['plugin_customfields']['device_type']['SoftwareLicense'] = 'Software License';
-$LANG['plugin_customfields']['device_type']['Device']          = $LANG['title'][30];
-
-$LANG['plugin_customfields']['component_type']['DeviceMotherboard']  = $LANG['devices'][5];
-$LANG['plugin_customfields']['component_type']['DeviceProcessor']    = $LANG['devices'][4];
-$LANG['plugin_customfields']['component_type']['DeviceMemory']       = $LANG['devices'][6];
-$LANG['plugin_customfields']['component_type']['DeviceHardDrive']    = $LANG['devices'][1];
-$LANG['plugin_customfields']['component_type']['DeviceNetworkCard']  = $LANG['devices'][3];
-$LANG['plugin_customfields']['component_type']['DeviceDrive']        = $LANG['devices'][19];
-$LANG['plugin_customfields']['component_type']['DeviceControl']      = $LANG['devices'][20];
-$LANG['plugin_customfields']['component_type']['DeviceGraphicCard']  = $LANG['devices'][2];
-$LANG['plugin_customfields']['component_type']['DeviceSoundCard']    = $LANG['devices'][7];
-$LANG['plugin_customfields']['component_type']['DevicePci']          = $LANG['devices'][21];
-$LANG['plugin_customfields']['component_type']['DeviceCase']         = $LANG['devices'][22];
-$LANG['plugin_customfields']['component_type']['DevicePowerSupply']  = $LANG['devices'][23];
-
 // Setup
 $LANG['plugin_customfields']['setup'][1] = 'Aucun champ restreint';
 $LANG['plugin_customfields']['setup'][2] = 'This plugin requires GLPI version 0.72 or higher';
@@ -142,4 +105,9 @@ $LANG['plugin_customfields']['setup'][10] = 'Are you sure you want to delete all
 $LANG['plugin_customfields']['setup'][11] = 'Instructions';
 //$LANG['plugin_customfields']['setup'][12] = 'FAQ';
 //$LANG['plugin_customfields']['setup'][14] = 'Please change to "Root Entity (Show all)" before installing this plugin';
+
+// Device Types
+$LANG['plugin_customfields']['device_type']['SoftwareVersion'] = 'Software Versions';
+$LANG['plugin_customfields']['device_type']['SoftwareLicense'] = 'Software License';
+include('devices.php');
 ?>
