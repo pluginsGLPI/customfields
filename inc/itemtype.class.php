96fbe0de15bc334248122f68fe91fc6a6ad85445
dethegeek <dethegeek@6f9ec73b-e5d2-495b-a2da-7e577d17d9e1>
2014-03-28 19:19:40 +0000

    When unregistering a itemtype, remove the fields definitions from the database. Not doing it make the custom fields for this itemtype magically reappear, while the column for their data is not recreated.

--- inc/itemtype.class.php
+++ inc/itemtype.class.php
@@ -140,14 +140,26 @@ class PluginCustomfieldsItemtype extends CommonDBTM
       $result = $DB->query($query);
       
       if ($DB->numrows($result) != 0) {
+         
+         // The itemtype must be disabled before unregistration
+         //  Has no effect if the itemtype is not enabled
+         plugin_customfields_disable_device($itemType);
           
          // The new item type needs to be unregistered
-         $enabled = 0;
+         
+         // Remove the fields definition for the itemtype being deleted
+         $query  = "DELETE FROM `glpi_plugin_customfields_fields`
+                    WHERE `itemtype` = '$itemType'";
+          
+         $result = $DB->query($query) or die($DB->error());;
+      
+         // Remove the itemtype from the itemtypes supported by the plugin 
          $query  = "DELETE FROM `glpi_plugin_customfields_itemtypes`
                     WHERE `itemtype` = '$itemType'";
           
-         $result = $DB->query($query);
-      
+         $result = $DB->query($query) or die($DB->error());;
+         
+         // Remove all data for the itemtype
          $table       = plugin_customfields_table($itemType);
          if ($table) {
             $query = "DROP TABLE IF EXISTS `$table`";
